1b8494f621189784228cd71908e1b54d12f3f12b
naveena-pillai <71680517+naveena-pillai@users.noreply.github.com>
2020-11-17 16:51:25 -0500

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,23 @@
+class Paper {
+constructor(x,y){
+    var options = {
+        'isStatic' : false,
+        'restitution' : 0.3,
+        'friction' : 0.7,
+        'density' : 1.2
+    }
+    this.body = Bodies.rectangle(x,y,20,20,options);
+    this.width = 20;
+    this.height = 20;
+
+    World.add(world, this.body);
+}
+display(){
+var pos = this.body.position;
+rectMode(CENTER);
+fill(92,102,247);
+rect(pos.x,pos.y,this.width, this.height);
+
+}
+
+};
